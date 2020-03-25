e37c314e1529eaaf679e1b61c53751d21f1379bc
Adil <templargin@gmail.com>
2020-03-25 16:56:13 -0400

    mobile responsive sidebar

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 $('#navigation').load("menu.html");
+
+
+function togNav() {
+  var bar = document.getElementById("mySidebar");
+  if (bar.style.display == "none"){
+    bar.style.display = "block";
+    document.getElementById("myHeader").style.marginLeft = "177px";
+    document.getElementById("myContent").style.marginLeft = "177px";
+  }
+  else{
+    bar.style.display = "none";
+    document.getElementById("myHeader").style.marginLeft = "20px";
+    document.getElementById("myContent").style.marginLeft = "20px";
+  }
+}
