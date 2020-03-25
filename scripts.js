0d40918e5d57705963ffea73e6f2350cbfebed23
Adil <templargin@gmail.com>
2020-03-25 17:56:14 -0400

    sidebar debugging

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 function togNav() {
   var bar = document.getElementById("mySidebar");
-  if (bar.style.display == "none"){
-    bar.style.display = "block";
+  if (bar.style.display == "none" || bar.style.display == ''){
     document.getElementById("myHeader").style.marginLeft = "177px";
     document.getElementById("myContent").style.marginLeft = "177px";
+    bar.style.display = "block";
   }
   else{
     bar.style.display = "none";
-    document.getElementById("myHeader").style.marginLeft = "20px";
-    document.getElementById("myContent").style.marginLeft = "20px";
+    document.getElementById("myHeader").style.marginLeft = "0";
+    document.getElementById("myContent").style.marginLeft = "0";
   }
 }
