364a18c932c35e41f5c37abf2fd088bedd7057b5
Adil <templargin@gmail.com>
2020-04-30 16:12:59 -0400

    styling

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function togNav() {
   var bar = document.getElementById("mySidebar");
   if (bar.style.display == "none" || bar.style.display == ''){
-    document.getElementById("myHeader").style.marginLeft = "177px";
-    document.getElementById("myContent").style.marginLeft = "177px";
+    document.getElementById("myHeader").style.marginLeft = "150px";
+    document.getElementById("myContent").style.marginLeft = "150px";
     bar.style.display = "block";
   }
   else{
